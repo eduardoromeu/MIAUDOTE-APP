445468f417f1f49a93ddadbc47330f34a4f73ff9
Dudu <eduardoromeueberts@gmail.com>
2025-09-21 15:37:48 -0300

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,7 @@
 import { index, route } from "@react-router/dev/routes";
 
 export default [
-	index("src/pages/home.jsx"),
+	index("src/pages/Home.jsx"),
   route("/search-pets", "src/pages/SearchPets.jsx"),
 	route("/register-pet", "src/pages/RegisterPet.jsx"),
   route("/success-stories", "src/pages/SuccessStories.jsx"),
