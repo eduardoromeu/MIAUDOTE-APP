94a9ae100c2ee2925e39b33372b0e5d107d82f93
Eugênio Domingues <inlabwall@gmail.com>
2025-09-25 22:54:39 -0300

    Refactor SideMenu component to remove unused imports and update navigation links

--- app/src/components/SideMenu/SideMenu.jsx
+++ app/src/components/SideMenu/SideMenu.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Box, Typography, Button, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
-// MUDANÇA 1: Removido o import do 'react-router-dom' que estava causando o erro.
 import PetsIcon from '@mui/icons-material/Pets';
-import SearchIcon from '@mui/icons-material/Search';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 
 function SideMenu() {
@@ -14,17 +12,8 @@ function SideMenu() {
       
       <List>
         <ListItem disablePadding>
-          {/* MUDANÇA 2: Trocamos component={Link} por component="a" e 'to' por 'href' */}
-          <ListItemButton component="a" href="/search-pets">
-            <ListItemIcon>
-              <SearchIcon />
-            </ListItemIcon>
-            <ListItemText primary="Buscar Pets" />
-          </ListItemButton>
-        </ListItem>
-        <ListItem disablePadding>
-          {/* MUDANÇA 3: Mesma troca aqui */}
-          <ListItemButton component="a" href="/my-favorites">
+          {/* MUDANÇA AQUI: O href agora aponta para /profile */}
+          <ListItemButton component="a" href="/profile"> 
             <ListItemIcon>
               <FavoriteIcon />
             </ListItemIcon>
@@ -41,7 +30,6 @@ function SideMenu() {
         variant='contained' 
         size='large' 
         fullWidth
-        // MUDANÇA 4: E aqui também
         component="a" 
         href="/register-pet"
         startIcon={<PetsIcon />}
